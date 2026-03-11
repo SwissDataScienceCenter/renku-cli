222c781cab7c5c682c034997779a6fc194bf4826
eikek <701128+eikek@users.noreply.github.com>
2026-03-11 10:33:46 +0100

    fix: Fix login command when using native-tls (#61)
    * Add oauth2 as explicit dependency
    
    For some reason that fixes the request to the openid discovery
    endpoint.
    
    * Propagate errors into snafu wrapper
    
    Instead of just the message, propagate the complete error structure
    
    * Fix dependency issues

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,5 +1,4 @@
-use assert_cmd::cargo::CargoError;
-use assert_cmd::prelude::*;
+use assert_cmd::cargo::{self, CargoError};
 use snafu::Snafu;
 use std::{io, process::Command};
 
@@ -31,7 +30,8 @@ impl std::convert::From<serde_json::Error> for Error {
 pub type Result<A> = std::result::Result<A, Error>;
 
 pub fn mk_cmd() -> Result<Command> {
-    let mut cmd = Command::cargo_bin("rnk")?;
-    cmd.args(["--renku-url", "https://ci-renku-3668.dev.renku.ch"]); //use mock url?
+    let prg = cargo::cargo_bin!("rnk");
+    let mut cmd = Command::new(prg);
+    cmd.args(["--renku-url", "https://dev.renku.ch"]); //use mock url?
     Ok(cmd)
 }
