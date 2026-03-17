1d544b344f4f7da299e09c2ed5ba73e51387014b
Ralf Grubenmann <ralf.grubenmann@sdsc.ethz.ch>
2026-03-17 13:56:00 +0100

    simplify release CI and create archives in releases (#62)
    * update release CI to match self_update and simplify yaml
    
    * add back concurrency option
    
    * adjust install script to deal with new tarfiles

--- build.rs
+++ build.rs
@@ -1,6 +1,10 @@
 use vergen::EmitBuilder;
 
 pub fn main() {
+    if let Ok(val) = std::env::var("RNK_RELEASE_VERSION") {
+        println!("cargo:rustc-env=CARGO_PKG_VERSION={}", val);
+    }
+    println!("cargo:rerun-if-env-changed=RNK_RELEASE_VERSION");
     EmitBuilder::builder()
         .all_build()
         .all_git()
