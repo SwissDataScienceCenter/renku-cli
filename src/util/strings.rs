1e7fe4ce98341a9892a1a286258478239516833d
Eike Kettner <eike.kettner@sdsc.ethz.ch>
2026-06-16 16:39:14 +0200

    Update rand crate

--- src/util/strings.rs
+++ src/util/strings.rs
@@ -1,12 +1,13 @@
-use rand::Rng;
+use rand::prelude::*;
 
 const CHARSET_ALPHA_NUM: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 const CHARSET_LOWER_ALPHA_NUM: &str = "abcdefghijklmnopqrstuvwxyz0123456789";
 const CHARSET_ALPHA: &str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 pub fn random(length: usize, charset: &str) -> String {
-    rand::thread_rng()
-        .sample_iter(&rand::distributions::Uniform::from(0..charset.len()))
+    let distr = rand::distr::Uniform::new(0, charset.len()).unwrap();
+    rand::rng()
+        .sample_iter(distr)
         .take(length)
         .map(|i| charset.chars().nth(i).unwrap())
         .collect()
