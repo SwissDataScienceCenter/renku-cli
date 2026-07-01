d5b055edad0f7c8183f18f2e43e637ff433550c9
Eike <eikek@posteo.de>
2026-07-01 13:27:58 +0200

    Update vergen to 9.1.0

--- build.rs
+++ build.rs
@@ -1,15 +1,19 @@
-use vergen::EmitBuilder;
+use vergen_gitcl::{BuildBuilder, CargoBuilder, Emitter, GitclBuilder, RustcBuilder};
 
 pub fn main() {
     if let Ok(val) = std::env::var("RNK_RELEASE_VERSION") {
         println!("cargo:rustc-env=CARGO_PKG_VERSION={}", val);
     }
     println!("cargo:rerun-if-env-changed=RNK_RELEASE_VERSION");
-    EmitBuilder::builder()
-        .all_build()
-        .all_git()
-        .all_cargo()
-        .all_rustc()
+    Emitter::default()
+        .add_instructions(&BuildBuilder::all_build().unwrap())
+        .unwrap()
+        .add_instructions(&CargoBuilder::all_cargo().unwrap())
+        .unwrap()
+        .add_instructions(&GitclBuilder::all_git().unwrap())
+        .unwrap()
+        .add_instructions(&RustcBuilder::all_rustc().unwrap())
+        .unwrap()
         .emit()
         .unwrap();
 }
