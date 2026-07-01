d328c61778072c573202167fce6f5e8847e2d430
Eike <eikek@posteo.de>
2026-07-01 15:41:16 +0200

    Update vergen to 10.0.1 and rust to 1.95

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-use vergen_gitcl::{BuildBuilder, CargoBuilder, Emitter, GitclBuilder, RustcBuilder};
+use vergen_gitcl::{Build, Cargo, Emitter, Gitcl, Rustc};
 
 pub fn main() {
     if let Ok(val) = std::env::var("RNK_RELEASE_VERSION") {
@@ -6,13 +6,13 @@ pub fn main() {
     }
     println!("cargo:rerun-if-env-changed=RNK_RELEASE_VERSION");
     Emitter::default()
-        .add_instructions(&BuildBuilder::all_build().unwrap())
+        .add_instructions(&Build::all_build())
         .unwrap()
-        .add_instructions(&CargoBuilder::all_cargo().unwrap())
+        .add_instructions(&Cargo::all_cargo())
         .unwrap()
-        .add_instructions(&GitclBuilder::all_git().unwrap())
+        .add_instructions(&Gitcl::all_git())
         .unwrap()
-        .add_instructions(&RustcBuilder::all_rustc().unwrap())
+        .add_instructions(&Rustc::all_rustc())
         .unwrap()
         .emit()
         .unwrap();
